7fc4e86e307fb194991a3aab2181f49c5eb52082
Martin Viidik <martvii@greeny.cs.tlu.ee>
2015-09-09 11:33:43 +0300

    First logic

--- helloworld.php
+++ helloworld.php
@@ -3,4 +3,21 @@
 	$last_name = "Viidik";
 	
 	echo $first_name." ".$last_name;
+?> 
+<br>
+<?php
+
+	$age = 18;
+	
+	// testib loogikat, juhul kui vanus on väiksem kui 18, siis kirjutab ""alaealine""
+	// kui 18 või üle, siis ""täisealine""
+	
+	if($age < 19 {
+		// tõene
+		echo "alaealine";
+		
+	} else {
+		// väär
+		echo "täisealine";
+	}
 ?>
